9526d8912f41391cb849208ddfde18fc88294423
francois-procab <francois@procab.ch>
2022-11-21 17:32:16 +0100

    Initial commit

---
+++ burger/burger.js
@@ -0,0 +1,18 @@
+const buttonsBurger = document.querySelectorAll(".burger");
+
+buttonsBurger.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		const currentState = button.getAttribute("data-state");
+		const target = button.getAttribute("aria-controls");
+
+		if (!currentState || currentState === "closed") {
+			button.setAttribute("data-state", "opened");
+			button.setAttribute("aria-expanded", "true");
+			document.querySelector(`#${target}`).classList.add("is-open");
+		} else {
+			button.setAttribute("data-state", "closed");
+			button.setAttribute("aria-expanded", "false");
+			document.querySelector(`#${target}`).classList.remove("is-open");
+		}
+	});
+});
