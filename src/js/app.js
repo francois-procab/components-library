2397d3d728fab493d6ae551320853833c968f1de
francois-procab <francois@procab.ch>
2023-02-22 17:07:06 +0100

    Add tooltip

--- src/js/app.js
+++ src/js/app.js
@@ -14,4 +14,24 @@ document.addEventListener("DOMContentLoaded", (e) => {
 	Modal;
 	Tabs;
 	Toast;
+
+	// TO MOVE
+	const hasSubNav = document.querySelectorAll(".has-sub-menu");
+	hasSubNav.forEach((item) => {
+		const trigger = item.querySelector(".menu-trigger");
+
+		trigger.addEventListener("click", (e) => {
+			e.preventDefault();
+
+			hasSubNav.forEach((elt) => {
+				elt.classList.remove("is-open");
+			});
+
+			if (item.classList.contains("is-open")) {
+				item.classList.remove("is-open");
+			} else {
+				item.classList.add("is-open");
+			}
+		});
+	});
 });
