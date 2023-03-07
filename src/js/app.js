26e11c9859e9507faf105f9de928b23b043adc42
francois-procab <francois@procab.ch>
2023-03-07 13:59:42 +0100

    Clean js toast component

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,4 @@
-import { Accordion } from "./components/accordion.js";
-import { ActiveLink } from "./components/active-link.js";
-import { Burger } from "./components/burger.js";
-import { Form } from "./components/form.js";
-// import { LoadMore } from "./components/load-more.js";
-import { Modal } from "./components/modal.js";
-import { Tabs } from "./components/tabs.js";
-import { Toast } from "./components/toast.js";
-
 document.addEventListener("DOMContentLoaded", (e) => {
-	Accordion;
-	ActiveLink;
-	Burger;
-	Form;
-	//LoadMore;
-	Modal;
-	Tabs;
-	Toast;
-
 	// TO MOVE
 	const hasSubNav = document.querySelectorAll(".has-sub-menu");
 	hasSubNav.forEach((item) => {
