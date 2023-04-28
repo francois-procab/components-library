cccab45e996ef2b0664c14edf9ba214b2ed14928
francois-procab <francois@procab.ch>
2023-04-28 17:03:19 +0200

    Add progress slide animation

--- src/js/layouts/cp-slider.js
+++ src/js/layouts/cp-slider.js
@@ -24,39 +24,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	let autoPlayDelay = 2500;
 
 	let mySwiper = Slider.initSwiper(sliderAutoFill, {
+		on: {
+			init: (el) => {
+				console.log(el);
+				el.pagination.el.style.setProperty("--_delay", `${autoPlayDelay}ms`);
+			},
+		},
 		slidesPerView: 1,
 		spaceBetween: 20,
+		watchSlidesProgress: true,
 		autoplay: {
 			delay: autoPlayDelay,
 		},
+		pagination: {
+			el: ".swiper-progress-bar",
+			clickable: true,
+		},
 	});
 
-	let slideCounts = mySwiper.params.loop ? mySwiper.slides.length - 2 : mySwiper.slides.length;
-	let widthParts = 100 / slideCounts;
-	console.log(widthParts);
-
-	for (let i = 0; i < slideCounts; i++) {
-		var div = document.createElement("div");
-		document.querySelector(".progress-sections").appendChild(div);
-	}
-
-	function initProgressBar() {
-		let calcProgress = (slideCounts - 1) * (autoPlayDelay + mySwiper.params.speed);
-		calcProgress += autoPlayDelay;
-		console.log(calcProgress);
-	}
-
-	initProgressBar();
-
-	mySwiper.on("slideChange", function () {
-		let progress = document.querySelector(".progress");
-
-		if ((this.progress == -0 || (this.progress == 1 && this.params.loop)) && !progress.parent().is(".stopped")) {
-			progress.style.width = 0;
-		}
-
-		if (progress.parentNode.classList.contains(".stopped")) {
-			progress.style.setProperty("--_w:", Math.round(widthParts * (this.activeIndex + 1)) + "%");
-		}
+	mySwiper.on("init", (el) => {
+		console.log(el);
 	});
 });
