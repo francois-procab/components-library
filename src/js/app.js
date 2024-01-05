d496632ea2bc47a8bbca416fedd04b47e345d50c
francois-procab <francois@procab.ch>
2024-01-05 14:37:18 +0100

    Test dploy

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,6 @@ import xml from "highlight.js/lib/languages/xml";
 import javascript from "highlight.js/lib/languages/javascript";
 import css from "highlight.js/lib/languages/css";
 
-// import { Toast } from "./components/toast.js";
-
 hljs.registerLanguage("xml", xml);
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("css", css);
